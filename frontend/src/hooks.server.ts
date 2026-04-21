f7c34e21f4c85881204921352603daab1e091cfd
Victor Hedegran <victor.hedegran@gmail.com>
2026-04-21 20:25:55 +0200

    Update handleFetch to use BACKEND_URL for request URL validation

--- frontend/src/hooks.server.ts
+++ frontend/src/hooks.server.ts
@@ -1,7 +1,8 @@
 import type { HandleFetch } from '@sveltejs/kit';
+import { BACKEND_URL } from '$env/static/private';
 
 export const handleFetch: HandleFetch = async ({ event, request, fetch }) => {
-	if (request.url.startsWith('http://127.0.0.1:3000')) {
+	if (request.url.startsWith(BACKEND_URL)) {
 		const cookie = event.request.headers.get('cookie');
 		if (cookie) {
 			request.headers.set('cookie', cookie);
