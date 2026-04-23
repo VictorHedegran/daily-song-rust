5f944408b6082f9f85d22d1ce42ea182f8b9245a
Victor Hedegran <victor.hedegran@gmail.com>
2026-04-23 19:17:41 +0200

    Fix template string syntax for BACKEND_URL in fetch request

--- frontend/src/routes/+layout.server.ts
+++ frontend/src/routes/+layout.server.ts
@@ -5,7 +5,7 @@ import { BACKEND_URL } from '$env/static/private';
 type Response<T> = { success: true; result: T } | { success: false; error: string };
 
 export const load: LayoutServerLoad = async ({ fetch }): Promise<Response<UserResponse>> => {
-	const res = await fetch('${BACKEND_URL}/me', {
+	const res = await fetch(`${BACKEND_URL}/me`, {
 		credentials: 'include'
 	});
 	if (!res.ok) return { success: false, error: res.statusText };
